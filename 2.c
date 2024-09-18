730188fde7aab3c5c489bf55acc35c7c0c2bfc56
SaidCarbot <141591969+SaidCarbot@users.noreply.github.com>
2024-09-18 10:58:58 -0600

    2.c

--- 2.c
+++ 2.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 
+int main() {
+    int tam, target;
 
-int main(){
-    int A[]={2, 7, 11, 15};
-    int target=9, tam;
+    printf("Ingrese el tamano del arreglo:");
+    scanf("%d", &tam);
 
-    for (int i=0;i<tam-1;i++){
+    int nums[tam];
+
+
+    printf("Ingrese los elementos del arreglo:\n");
+    for (int i=0;i<tam;i++){
+        printf("Elemento %d:", i+1);
+        scanf("%d", &nums[i]);
+    }
+
+
+    printf("Ingrese el valor de target:");
+    scanf("%d", &target);
+
+
+    printf("El arreglo es: ");
+    for(int i=0;i<tam;i++){
+        printf("%d", nums[i]);
+    }
+    printf("\n");
+
+
+    for(int i=0;i<tam-1;i++){
         for(int j=i+1;j<tam;j++){
-            if(A[i]+A[j]==target){
-
-                printf("Los indices son: %d y %d\n",i,j);
-                return;
+            if(nums[i]+nums[j]==target){
+                printf("Los indices son: %d y %d\n", i, j);
+                return 0;
             }
         }
     }
+
+    printf("No se encontraron dos n�meros que sumen %d\n", target);
     return 0;
-    }
+}
