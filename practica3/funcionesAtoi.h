e337ff808e265ecba2270fd8d37fe26ebb2b1ece
SaidCarbot <141591969+SaidCarbot@users.noreply.github.com>
2024-10-01 17:50:09 -0600

    Create funcionesAtoi.h

---
+++ practica3/funcionesAtoi.h
@@ -0,0 +1,17 @@
+/*
+1st, oct.-2024
+Del que contienen solo los nombres de las funciones
+*/
+#ifndef STRUCTS
+#define STRUCTS
+
+#define CAPACITY 200 // tamaño máximo del mensaje
+
+
+int myAtoi(unsigned char *cadena);                  // A.1
+void eliminarEspacios(unsigned char *cadena);       // A.2
+void limpiar_buffer();                              // A.3
+
+#include "functionsAtoi.c"
+#endif
+// Por Said Carbot & Elian, ESCOM-IPN-MEX 1-10-24
